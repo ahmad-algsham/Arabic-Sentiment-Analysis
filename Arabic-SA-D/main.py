ca1d226b6c5aedd4b060a9834e8ecdfe4f3f5ae1
ahmad-algsham <47936627+ahmad-algsham@users.noreply.github.com>
2020-03-09 00:05:55 +0300

    ad function __name__

--- Arabic-SA-D/main.py
+++ Arabic-SA-D/main.py
@@ -1,6 +1,5 @@
 import regexarabic as ra
 import craweltweet as ct
-# from craweltweet import *
 
 
 # --------------------------------------import from craweltweet---------------------------------------------------------
@@ -14,30 +13,32 @@ ct.data_trend(file_name_data_sa="DATA_SA", file_name_data_ae="DATA_AE", file_nam
               file_name_data_qa="DATA_QA", file_name_data_bh="DATA_BH", file_name_data_kw="DATA_KW",)
 
 # --------------------------------------import from regexarabic---------------------------------------------------------
+def main():
+    # Function to extract data based on top trend on each gulf states on one DataFrame
+    ct.clean_ALL['clean_tweet_SA'] = ct.df_SA['Tweets'].apply(lambda x: ra.remove(ra.harakat(ra.WordsFiltires(x))))
+    ct.clean_ALL['clean_tweet_SA'].to_csv('clean_SA.csv', encoding='utf-16', sep='\t', index=False)
+    print('Done')
 
-# Function to extract data based on top trend on each gulf states on one DataFrame
-ct.clean_ALL['clean_tweet_SA'] = ct.df_SA['Tweets'].apply(lambda x: ra.remove(ra.harakat(ra.WordsFiltires(x))))
-ct.clean_ALL['clean_tweet_SA'].to_csv('clean_SA.csv', encoding='utf-16', sep='\t', index=False)
-print('Done')
+    ct.clean_ALL['clean_tweet_AE'] = ct.df_AE['Tweets'].apply(lambda x: ra.remove(ra.harakat(ra.WordsFiltires(x))))
+    ct.clean_ALL['clean_tweet_AE'].to_csv('clean_AE.csv', encoding='utf-16', sep='\t', index=False)
+    print('Done')
 
-ct.clean_ALL['clean_tweet_AE'] = ct.df_AE['Tweets'].apply(lambda x: ra.remove(ra.harakat(ra.WordsFiltires(x))))
-ct.clean_ALL['clean_tweet_AE'].to_csv('clean_AE.csv', encoding='utf-16', sep='\t', index=False)
-print('Done')
+    ct.clean_ALL['clean_tweet_OM'] = ct.df_OM['Tweets'].apply(lambda x: ra.remove(ra.harakat(ra.WordsFiltires(x))))
+    ct.clean_ALL['clean_tweet_OM'].to_csv('clean_OM.csv', encoding='utf-16', sep='\t', index=False)
+    print('Done')
 
-ct.clean_ALL['clean_tweet_OM'] = ct.df_OM['Tweets'].apply(lambda x: ra.remove(ra.harakat(ra.WordsFiltires(x))))
-ct.clean_ALL['clean_tweet_OM'].to_csv('clean_OM.csv', encoding='utf-16', sep='\t', index=False)
-print('Done')
+    ct.clean_ALL['clean_tweet_QA'] = ct.df_QA['Tweets'].apply(lambda x: ra.remove(ra.harakat(ra.WordsFiltires(x))))
+    ct.clean_ALL['clean_tweet_QA'].to_csv('clean_QA.csv', encoding='utf-16', sep='\t', index=False)
+    print('Done')
 
-ct.clean_ALL['clean_tweet_QA'] = ct.df_QA['Tweets'].apply(lambda x: ra.remove(ra.harakat(ra.WordsFiltires(x))))
-ct.clean_ALL['clean_tweet_QA'].to_csv('clean_QA.csv', encoding='utf-16', sep='\t', index=False)
-print('Done')
+    ct.clean_ALL['clean_tweet_BH'] = ct.df_BH['Tweets'].apply(lambda x: ra.remove(ra.harakat(ra.WordsFiltires(x))))
+    ct.clean_ALL['clean_tweet_BH'].to_csv('clean_BH.csv', encoding='utf-16', sep='\t', index=False)
+    print('Done')
 
-ct.clean_ALL['clean_tweet_BH'] = ct.df_BH['Tweets'].apply(lambda x: ra.remove(ra.harakat(ra.WordsFiltires(x))))
-ct.clean_ALL['clean_tweet_BH'].to_csv('clean_BH.csv', encoding='utf-16', sep='\t', index=False)
-print('Done')
-
-ct.clean_ALL['clean_tweet_KW'] = ct.df_KW['Tweets'].apply(lambda x: ra.remove(ra.harakat(ra.WordsFiltires(x))))
-ct.clean_ALL['clean_tweet_KW'].to_csv('clean_KW.csv', encoding='utf-16', sep='\t', index=False)
-print('Done')
+    ct.clean_ALL['clean_tweet_KW'] = ct.df_KW['Tweets'].apply(lambda x: ra.remove(ra.harakat(ra.WordsFiltires(x))))
+    ct.clean_ALL['clean_tweet_KW'].to_csv('clean_KW.csv', encoding='utf-16', sep='\t', index=False)
+    print('Done')
 
 # ----------------------------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+    main()
