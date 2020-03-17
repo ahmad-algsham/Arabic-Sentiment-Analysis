87f2dc5293a29fdecab752d29caf500baa51f8a7
ahmad-algsham <47936627+ahmad-algsham@users.noreply.github.com>
2020-03-17 05:16:23 +0300

    self improve final pro (added one call state)

--- ASAD/main_streamer.py
+++ ASAD/main_streamer.py
@@ -4,9 +4,16 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
+from textblob import TextBlob
+
 import kays_twitter
-import pandas as pd
+import regexarabic as ra
+import functions as ff
+import gulfstates as gs
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import re
 
 
 # # # # TWITTER CLIENT # # # #
@@ -16,33 +23,14 @@ class TwitterClient():
         self.twitter_client = API(self.auth)
 
         self.twitter_user = twitter_user
+
     def get_twitter_client_api(self):
         return self.twitter_client
 
-    def get_user_timeline_tweets(self, num_tweets):
-        tweets = []
-        for tweet in Cursor(self.twitter_client.user_timeline, id=self.twitter_user).items(num_tweets):
-            tweets.append(tweet)
-        return tweets
 
-    def get_friend_lsit(self, num_friends):
-        friend_list = []
-        for friend in Cursor(self.twitter_client.friends, id=self.twitter_user).items(num_friends):
-            friend_list.append(friend)
-        return friend_list
-
-    def get_home_timeline_tweets(self, num_tweets):
-        home_timeline_tweets = []
-        for tweet in Cursor(self.twitter_client.home_timeline, id=self.twitter_user).items(num_tweets):
-            home_timeline_tweets.append(tweet)
-        return home_timeline_tweets
-
-
-# # # # TWITTER AUTHENTICATION # # # #
+# # # # TWITTER AUTHENTICATER # # # #
 class TwitterAuthenticator():
-    """
-    class for authentications TWITTER API
-    """
+
     def authenticate_twitter_app(self):
         auth = OAuthHandler(kays_twitter.consumer_key, kays_twitter.consumer_secret)
         auth.set_access_token(kays_twitter.access_key, kays_twitter.access_secret)
@@ -52,89 +40,94 @@ class TwitterAuthenticator():
 # # # # TWITTER STREAMER # # # #
 class TwitterStreamer():
     """
-    class for streaming and processing live tweet
+    Class for streaming and processing live tweets.
     """
-    def __init__(self):
-        self.twitter_authenticator = TwitterAuthenticator()
 
-    def stream_tweets(self, fetched_tweets_filename, hash_tag_lsit):
-        # this handles Twitter authentication and the connection to the Twitter Streaming API
+    def __init__(self):
+        self.twitter_autenticator = TwitterAuthenticator()
+
+    def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
+        # This handles Twitter authetification and the connection to Twitter Streaming API
         listener = TwitterListener(fetched_tweets_filename)
-        auth = self.twitter_authenticator.authenticate_twitter_app()
+        auth = self.twitter_autenticator.authenticate_twitter_app()
         stream = Stream(auth, listener)
 
-        # This line filter Twitter Streams to capture data by the keywords
-        stream.filter(track=hash_tag_lsit)
+        # This line filter Twitter Streams to capture data by the keywords:
+        stream.filter(track=hash_tag_list)
 
 
 # # # # TWITTER STREAM LISTENER # # # #
 class TwitterListener(StreamListener):
-    """"
-    This a basic listener class that just prints received tweets to stdout.
+    """
+    This is a basic listener that just prints received tweets to stdout.
     """
 
     def __init__(self, fetched_tweets_filename):
         self.fetched_tweets_filename = fetched_tweets_filename
 
-    def on_data(self, raw_data):
+    def on_data(self, data):
         try:
-            print(raw_data)
-            with open(self.fetched_tweets_filename, 'a', encoding='utf-8') as tf:
-                tf.write(raw_data)
+            print(data)
+            with open(self.fetched_tweets_filename, 'a') as tf:
+                tf.write(data)
             return True
         except BaseException as e:
-            print("Error on_data: %s" % str(e))
+            print("Error on_data %s" % str(e))
         return True
 
-    def on_error(self, status_code):
-        if status_code == 420:
-            # Return False on_data method in case rate limit occurs.
+    def on_error(self, status):
+        if status == 420:
+            # Returning False on_data method in case rate limit occurs.
             return False
-        print(status_code)
+        print(status)
 
 
 class TweetAnalyzer():
     """
-    Functionality for analysis and categorizing content from tweets.
+    Functionality for analyzing and categorizing content from tweets.
     """
-    def tweets_to_data_frame(self, tweets):
-        df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
-        return df
+
+    def clean_tweet(self, tweet):
+        argword = ra.remove(tweet)
+        argword = ra.harakat(argword)
+        argword = ra.WordsFiltires(argword)
+        return argword
+        # return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+    def analyze_sentiment(self, tweet):
+        analysis = TextBlob(self.clean_tweet(tweet))
+
+        if analysis.sentiment.polarity > 0:
+            return 1
+        elif analysis.sentiment.polarity == 0:
+            return 0
+        else:
+            return -1
+
+    def tweets_to_data_frame(self, result_SA):
+        df_SA = gs.get_saudi_arabia(result_SA)
+
+        return df_SA
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     twitter_client = TwitterClient()
+    tweet_analyzer = TweetAnalyzer()
     api = twitter_client.get_twitter_client_api()
 
-    tweets = api.user_timeline(screan_name="News_Ejazah", count=10)
-    print()
+    result_SA = api.trends_place(23424938)
+
+    df_SA = tweet_analyzer.tweets_to_data_frame(result_SA)
+    df_SA['sentiment'] = df_SA['Tweets'].apply(lambda x: tweet_analyzer.analyze_sentiment(x))
+    df_SA.to_csv('data_SA.csv', encoding='utf-16', sep='\t', index=False)
+
+
+    print(df_SA.head(10))
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # hash_tag_lsit = ['#فيروس_كورونا', '#علمتني_كورونا']
-    # fetched_tweets_filename = 'tweets.txt'
-    #
-    # twitter_client = TwitterClient('News_Ejazah')
-    # print(twitter_client.get_user_timeline_tweets(1))
-    # print(twitter_client.get_friend_lsit(1))
-    # print(twitter_client.get_home_timeline_tweets(1))
-    #
-    # # twitter_Streamer = TwitterStreamer()
-    # # twitter_Streamer.stream_tweets(fetched_tweets_filename, hash_tag_lsit)
+    # twitter_client = TwitterClient()
+    # print(twitter_client.get_user_timeline_tweets(1, result_SA))
