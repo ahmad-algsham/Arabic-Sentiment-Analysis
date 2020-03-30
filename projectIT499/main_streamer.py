34bb214a246659dcbc435dd7dd3a38dca2d569e4
ahmad-algsham <47936627+ahmad-algsham@users.noreply.github.com>
2020-03-30 08:44:24 +0300

    set the rate limit and add while loop and try except

--- projectIT499/main_streamer.py
+++ projectIT499/main_streamer.py
@@ -100,87 +100,87 @@ if __name__ == '__main__':
     tweet_analyzer = TweetAnalyzer()
     api = twitter_client.get_twitter_client_api()
 
-    # while True:
-    try:
-        # extract tweets from top trend in Saudi Arabia
-        df_SA = twitter_client.get_saudi_arabia()
-        print('Start sa sentiment')
-        df_SA['sentiment'] = df_SA['Tweets'].apply(lambda x: tweet_analyzer.analyze_sentiment(x))
-        df_SA.to_csv('data_SA.csv', encoding='utf-16', sep='\t', index=False)
-        print('waiting for avoid rate limit')
-        time.sleep(100)  # Google Translate API also has a default limit  100,000 characters per 100 second.
-    except BaseException as e:
-        print("Error get_saudi_arabia %s" % str(e))
-        time.sleep(1)
-        pass
+    while True:
+        try:
+            # extract tweets from top trend in Saudi Arabia
+            df_SA = twitter_client.get_saudi_arabia()
+            print('Start sa sentiment')
+            df_SA['sentiment'] = df_SA['Tweets'].apply(lambda x: tweet_analyzer.analyze_sentiment(x))
+            df_SA.to_csv('data_SA.csv', encoding='utf-16', sep='\t', index=False)
+            print('waiting for avoid rate limit')
+            time.sleep(100)  # Google Translate API also has a default limit  100,000 characters per 100 second.
+        except BaseException as e:
+            print("Error get_saudi_arabia %s" % str(e))
+            time.sleep(1)
+            pass
 
-    try:
-        # extract tweets from top trend in Kuwait
-        df_KW = twitter_client.get_kuwait()
-        print("Start kw sentiment")
-        df_KW['sentiment'] = df_KW['Tweets'].apply(lambda x: tweet_analyzer.analyze_sentiment(x))
-        df_KW.to_csv('data_KW.csv', encoding='utf-16', sep='\t', index=False)
-        print('waiting for avoid rate limit')
-        time.sleep(100)  # Google Translate API also has a default limit  100,000 characters per 100 second.
-    except BaseException as e:
-        print("Error get_kuwait %s" % str(e))
-        time.sleep(1)
-        pass
+        try:
+            # extract tweets from top trend in Kuwait
+            df_KW = twitter_client.get_kuwait()
+            print("Start kw sentiment")
+            df_KW['sentiment'] = df_KW['Tweets'].apply(lambda x: tweet_analyzer.analyze_sentiment(x))
+            df_KW.to_csv('data_KW.csv', encoding='utf-16', sep='\t', index=False)
+            print('waiting for avoid rate limit')
+            time.sleep(100)  # Google Translate API also has a default limit  100,000 characters per 100 second.
+        except BaseException as e:
+            print("Error get_kuwait %s" % str(e))
+            time.sleep(1)
+            pass
 
-    try:
-        # extract tweets from top trend in Bahrain
-        df_BH = twitter_client.get_bahrain()
-        print('Start bh sentiment')
-        df_BH['sentiment'] = df_BH['Tweets'].apply(lambda x: tweet_analyzer.analyze_sentiment(x))
-        df_BH.to_csv('data_BH.csv', encoding='utf-16', sep='\t', index=False)
-        print('waiting for avoid rate limit')
-        time.sleep(100)  # Google Translate API also has a default limit  100,000 characters per 100 second.
-    except BaseException as e:
-        print("Error get_bahrain %s" % str(e))
-        time.sleep(1)
-        pass
+        try:
+            # extract tweets from top trend in Bahrain
+            df_BH = twitter_client.get_bahrain()
+            print('Start bh sentiment')
+            df_BH['sentiment'] = df_BH['Tweets'].apply(lambda x: tweet_analyzer.analyze_sentiment(x))
+            df_BH.to_csv('data_BH.csv', encoding='utf-16', sep='\t', index=False)
+            print('waiting for avoid rate limit')
+            time.sleep(100)  # Google Translate API also has a default limit  100,000 characters per 100 second.
+        except BaseException as e:
+            print("Error get_bahrain %s" % str(e))
+            time.sleep(1)
+            pass
 
-    try:
-        # extract tweets from top trend in Qatar
-        df_QA = twitter_client.get_qatar()
-        print('Start qa sentiment')
-        df_QA['sentiment'] = df_QA['Tweets'].apply(lambda x: tweet_analyzer.analyze_sentiment(x))
-        df_QA.to_csv('data_QA.csv', encoding='utf-16', sep='\t', index=False)
-        print('waiting for avoid rate limit')
-        time.sleep(100)  # Google Translate API also has a default limit  100,000 characters per 100 second.
-    except BaseException as e:
-        print("Error get_qatar %s" % str(e))
-        time.sleep(1)
-        pass
+        try:
+            # extract tweets from top trend in Qatar
+            df_QA = twitter_client.get_qatar()
+            print('Start qa sentiment')
+            df_QA['sentiment'] = df_QA['Tweets'].apply(lambda x: tweet_analyzer.analyze_sentiment(x))
+            df_QA.to_csv('data_QA.csv', encoding='utf-16', sep='\t', index=False)
+            print('waiting for avoid rate limit')
+            time.sleep(100)  # Google Translate API also has a default limit  100,000 characters per 100 second.
+        except BaseException as e:
+            print("Error get_qatar %s" % str(e))
+            time.sleep(1)
+            pass
 
-    try:
-        # extract tweets from top trend in United Arab Emirates
-        df_AE = twitter_client.get_united_arab_emirates()
-        print('Start ae sentiment')
-        df_AE['sentiment'] = df_AE['Tweets'].apply(lambda x: tweet_analyzer.analyze_sentiment(x))
-        df_AE.to_csv('data_AE.csv', encoding='utf-16', sep='\t', index=False)
-        print('waiting for avoid rate limit')
-        time.sleep(100)  # Google Translate API also has a default limit  100,000 characters per 100 second.
-    except BaseException as e:
-        print("Error get_united_arab_emirates %s" % str(e))
-        time.sleep(1)
-        pass
+        try:
+            # extract tweets from top trend in United Arab Emirates
+            df_AE = twitter_client.get_united_arab_emirates()
+            print('Start ae sentiment')
+            df_AE['sentiment'] = df_AE['Tweets'].apply(lambda x: tweet_analyzer.analyze_sentiment(x))
+            df_AE.to_csv('data_AE.csv', encoding='utf-16', sep='\t', index=False)
+            print('waiting for avoid rate limit')
+            time.sleep(100)  # Google Translate API also has a default limit  100,000 characters per 100 second.
+        except BaseException as e:
+            print("Error get_united_arab_emirates %s" % str(e))
+            time.sleep(1)
+            pass
 
-    try:
-        # extract tweets from top trend in Oman
-        df_OM = twitter_client.get_oman()
-        print('Start om sentiment')
-        df_OM['sentiment'] = df_OM['Tweets'].apply(lambda x: tweet_analyzer.analyze_sentiment(x))
-        df_OM.to_csv('data_OM.csv', encoding='utf-16', sep='\t', index=False)
-        print('waiting for avoid rate limit')
-        # Google Translate API also has a default limit  2 million characters per day.
-    except BaseException as e:
-        print("Error get_oman %s" % str(e))
-        time.sleep(1)
-        pass
+        try:
+            # extract tweets from top trend in Oman
+            df_OM = twitter_client.get_oman()
+            print('Start om sentiment')
+            df_OM['sentiment'] = df_OM['Tweets'].apply(lambda x: tweet_analyzer.analyze_sentiment(x))
+            df_OM.to_csv('data_OM.csv', encoding='utf-16', sep='\t', index=False)
+            print('waiting for avoid rate limit')
+            # Google Translate API also has a default limit  2 million characters per day.
+        except BaseException as e:
+            print("Error get_oman %s" % str(e))
+            time.sleep(1)
+            pass
 
-    print('Done for now')
-    print('redo the process')
-        # time.sleep(100)
+        print('Done for now')
+        print('redo the process')
+        time.sleep(100)
 
     # TODO learn about matplotlib
